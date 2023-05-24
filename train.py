6a6bd55114c333483a45daf1ef7d6ec04808a3ec
abrilpm <118255304+abrilpm@users.noreply.github.com>
2023-05-24 08:52:55 +0200

    Creation train.py
    First implementation of the training function for our problem

--- train.py
+++ train.py
@@ -1,44 +1,39 @@
-from tqdm.auto import tqdm
 import wandb
+import torch
+import torchvision
+import numpy as np
+from torch import nn
+from Preprocessing.DataClass import *
+from Preprocessing.LoaderClass import *
+from Preprocessing.ColorProcessing import *
+from Models.Model1 import *
 
-def train(model, loader, criterion, optimizer, config):
-    # Tell wandb to watch what the model gets up to: gradients, weights, and more!
-    wandb.watch(model, criterion, log="all", log_freq=10)
-
-    # Run training and track with wandb
-    total_batches = len(loader) * config.epochs
-    example_ct = 0  # number of examples seen
+def train(model, loader, criterion, optimizer, num_epochs):
+    #wandb.init
+    #wandb.watch(model, criterion, log='all', log_freq=10)
+    train_loss = 0
+    model.train()
+    example_ct = 0
     batch_ct = 0
-    for epoch in tqdm(range(config.epochs)):
-        for _, (images, labels) in enumerate(loader):
-
-            loss = train_batch(images, labels, model, optimizer, criterion)
-            example_ct +=  len(images)
+    for epoch in range(num_epochs):
+        i = 0
+        for batch in loader:
+            optimizer.zero_grad()
+            greys = loader.get_batch_greys(i)
+            label = loader.get_batch_labels(i)
+            outs = model(greys)
+            train_loss = criterion(outs, label)
+            train_loss.backward()
+            optimizer.step()
+            example_ct += len(batch)
             batch_ct += 1
+            i += 1
 
-            # Report metrics every 25th batch
-            if ((batch_ct + 1) % 25) == 0:
-                train_log(loss, example_ct, epoch)
-
-
-def train_batch(images, labels, model, optimizer, criterion, device="cuda"):
-    images, labels = images.to(device), labels.to(device)
-    
-    # Forward pass ➡
-    outputs = model(images)
-    loss = criterion(outputs, labels)
-    
-    # Backward pass ⬅
-    optimizer.zero_grad()
-    loss.backward()
-
-    # Step with optimizer
-    optimizer.step()
-
-    return loss
+            #if ((batch_ct + 1) % 25) == 0:
+            #    train_log(train_loss, example_ct, epoch)
+        print ("Epoch: " + str(epoch) + " | Loss: " + str(train_loss))
 
 
 def train_log(loss, example_ct, epoch):
-    # Where the magic happens
     wandb.log({"epoch": epoch, "loss": loss}, step=example_ct)
     print(f"Loss after {str(example_ct).zfill(5)} examples: {loss:.3f}")
