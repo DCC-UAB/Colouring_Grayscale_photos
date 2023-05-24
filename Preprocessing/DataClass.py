da338812766295c885e5398ae10260d51543c14f
abrilpm <118255304+abrilpm@users.noreply.github.com>
2023-05-24 09:04:45 +0200

    Update DataClass.py
    Class LabImage added to DataClass

--- Preprocessing/DataClass.py
+++ Preprocessing/DataClass.py
@@ -1,7 +1,9 @@
 import os
 import random
+import torch
 from PIL import Image
 from torch.utils.data import Dataset
+from Preprocessing.ColorProcessing import *
 
 class DataClass(Dataset):
     def __init__(self, image_path, transform=None):
@@ -21,3 +23,42 @@ class DataClass(Dataset):
     
     def shuffle(self):
         random.shuffle(self.total_imgs)
+
+        
+class LabImage():
+    def __init__(self, dataclass):
+        self.dataclass = dataclass
+        self.lab_images = list()
+        self.L = list()
+        self.ab = list()
+        for i in range(len(self.dataclass)):
+            image = TransformToLAB(self.dataclass[i])
+            self.lab_images.append(image)
+            self.L.append(image[0])
+            self.ab.append(image[1:3])
+
+    def __getitem__(self, idx):
+        return (self.L[idx], self.ab[idx])
+    
+    def __len__(self):
+        return len(self.lab_images)
+    
+    def split_channels(self):
+        self.L = list()
+        self.ab = list()
+        for image in self.lab_images:
+            self.L.append(torch.unsqueeze(image[0],0))
+            self.ab.append(image[1:3])
+    
+    def shuffle(self):
+        random.shuffle(self.lab_images)
+        self.split_channels()
+
+    def get_grey(self, idx):
+        return self.L[idx]
+    
+    def get_ab(self, idx):
+        return self.ab[idx]
+    
+    def get_LAB_image(self, idx):
+        return self.lab_images[idx]
