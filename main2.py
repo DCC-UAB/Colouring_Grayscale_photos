b63702cd039b96a4f03574544bb1502d819ef810
abrilpm <118255304+abrilpm@users.noreply.github.com>
2023-05-27 19:58:48 +0200

    Update main2.py
    Coments added

--- main2.py
+++ main2.py
@@ -12,6 +12,7 @@ from models.ConvAE2 import *
 from train import *
 from plots import *
 
+#Function to initialize the weights using Xavier initialization
 def init_parameters(model):
   for name, w in model.named_parameters():
     if "weight" in name:
@@ -23,33 +24,16 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 if __name__ == '__main__':
     #wandb.init
+    #Transformation aplied to the images
     trans = torchvision.transforms.ToTensor()
-    trans2 = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize([0.4336, 0.4392, 0.4503], [0.2467, 0.2137, 0.2168])
-    ])
         
-    faces_path = './LandscapeDataset'
-    dataset = DataClass(faces_path, transform=trans)
+    faces_path = './LandscapeDataset' #Images path
+    dataset = DataClass(faces_path, transform=trans) #Initialization of the dataset
+    dataloader = LoaderClass(dataset, 128) #Initialization of the dataloader
 
-    dataloader = LoaderClass(dataset, 128)
-    print("Dataloader done")
-    model = ConvAE2().to(device)
-    init_parameters(model)
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay = 0.0)
-    criterion = nn.MSELoss()
-    train(model, dataloader, criterion, optimizer, 20)
-
-
-    #PREDICT 1 IMAGE
-    image = dataset[0]
-    grey = image[0]
-    pred = model(grey.to(device)).to('cpu')
-    print(pred)
-    print(image[1])
-    colored_image_Lab = torch.cat([grey, pred], dim=0).detach()
-    original_image_Lab = torch.cat([grey, image[1]], dim=0)
-    colored_RGB = TransformToRGB(colored_image_Lab)
-    original_RGB = TransformToRGB(original_image_Lab)
-    showImage(colored_RGB,'./imatgesProva/pred')
-    showImage(original_RGB,'./imatgesProva/original')
+    model = ConvAE2().to(device) #Initialization of the model
+    init_parameters(model) #Xavier initialization of the weights
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay = 0.0) #Optimizer initialization
+    criterion = nn.MSELoss() #Criterion initialization
+    
+    train(model, dataloader, criterion, optimizer, 20) #Training of the model
